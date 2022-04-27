14eaa5971f6f27e3e189468f5fbdce7f5d574b4b
hisham <H.Ibraheem@abuodeh.com>
2022-04-27 15:06:23 +0300

    edit login

--- pages/account/login.js
+++ pages/account/login.js
@@ -1,32 +1,53 @@
 import { useState } from 'react'
+import { FcCheckmark, FcHighPriority } from 'react-icons/fc'
 
 function Login() {
   const [addClass, setAddClass] = useState('')
   const [username, setUsername] = useState('')
+  const [usedUser, setUsedUser] = useState(false)
+  const [wrongUser,setWrongUser] = useState(false)
   const [email, setEmail] = useState('')
+  const [usedEmail, setUsedEmail] = useState(false)
   const [password, setPassword] = useState('')
+  const [wrongPass,setWrongPass] = useState(false)
   const [confirmPass, setConfirmPass] = useState('')
-  const [match, setMatch] = useState('')
+  const [match, setMatch] = useState(false)
+  const [hide, setHide] = useState(true)
   const emptyFields = () => {
-    console.log(username, password, email, confirmPass)
     setUsername('')
     setEmail('')
     setPassword('')
     setConfirmPass('')
+    setMatch(false)
+    setHide(true)
   }
   const validate = () => {
     if (confirmPass.length > 0 && password.length > 0) {
-      if (confirmPass == password) {
-        setMatch('matched')
+      if (confirmPass === password) {
+        setHide(false)
+        setMatch(true)
       } else {
-        setMatch('not matched')
+        setHide(false)
+        setMatch(false)
       }
+    } else {
+      setMatch(false)
+      setHide(true)
     }
   }
-  const changeUser = () => {}
-  const changePassword = () => {}
-  const login = () => {}
-
+  const register = () => {
+    const data = {
+      username : username,
+      email : email,
+      password : password
+    }
+  }
+  const login = () => {
+    const data = {
+      username : username,
+      password : password
+    }
+  }
   return (
     <div className="login-reg-container">
       <div className="loginbody">
@@ -34,67 +55,127 @@ function Login() {
           <div className="form-container sign-up-container">
             <form action="#">
               <h1>Create Account</h1>
-              <input
-                type="text"
-                placeholder="Username"
-                value={username}
-                onChange={(event) => {
-                  setUsername(event.target.value)
-                }}
-              />
-              <input
-                type="email"
-                placeholder="Email"
-                value={email}
-                onChange={(event) => {
-                  setEmail(event.target.value)
-                }}
-              />
-              <input
-                type="password"
-                placeholder="Password"
-                value={password}
-                pattern="(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{8,}"
-                onChange={(event) => {
-                  setPassword(event.target.value)
-                }}
-              />
-              <input
-                type="password"
-                placeholder="Confirm Password"
-                onBlur={validate}
-                value={confirmPass}
-                onChange={(event) => {
-                  setConfirmPass(event.target.value)
-                }}
-              />
-              <div className="w-full text-left">
-                <div className='text-xs text-green-500'>{match}</div>
+              <div className="relative h-fit w-full">
+                <input
+                  type="text"
+                  placeholder="Username"
+                  value={username}
+                  onFocus={() => {
+                    if(usedUser){
+                      setUsedUser(true)
+                    }
+                  }}
+                  onChange={(event) => {
+                    setUsername(event.target.value)
+                  }}
+                />
+                <div className="absolute inset-y-1/3 right-4 mt-1 h-1/5 w-3">
+                  {!usedUser ? <></> : <FcHighPriority />}
+                </div>
               </div>
-              <button>Sign Up</button>
+              <div className="relative h-fit w-full">
+                <input
+                  type="email"
+                  placeholder="Email"
+                  value={email}
+                  onFocus={() => {
+                    if(usedEmail){
+                      setUsedEmail(true)
+                    }
+                  }}
+                  onChange={(event) => {
+                    setEmail(event.target.value)
+                  }}
+                />
+                <div className="absolute inset-y-1/3 right-4 mt-1 h-1/5 w-3">
+                  {!usedEmail ? <></> : <FcHighPriority />}
+                </div>
+              </div>
+              <div className="relative h-fit w-full">
+                <input
+                  type="password"
+                  placeholder="Password"
+                  value={password}
+                  pattern="(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{8,}"
+                  onBlur={validate}
+                  onChange={(event) => {
+                    setHide(true)
+                    setPassword(event.target.value)
+                  }}
+                />
+                {/* <div className="absolute inset-y-1/3 right-4 mt-1 h-1/5 w-3">
+                  {hide ? (
+                    <></>
+                  ) : (
+                    <>{match ? <FcCheckmark /> : <FcHighPriority />}</>
+                  )}
+                </div> */}
+              </div>
+              <div className="relative h-fit w-full">
+                <input
+                  type="password"
+                  placeholder="Confirm Password"
+                  onBlur={validate}
+                  onFocus={() => {
+                    setHide(true)
+                  }}
+                  value={confirmPass}
+                  onChange={(event) => {
+                    setConfirmPass(event.target.value)
+                  }}
+                />
+                <div className="absolute inset-y-1/3 right-4 mt-1 h-1/5 w-3">
+                  {hide ? (
+                    <></>
+                  ) : (
+                    <>{match ? <FcCheckmark /> : <FcHighPriority />}</>
+                  )}
+                </div>
+              </div>
+              <button onClick={register}>Sign Up</button>
             </form>
           </div>
           <div className="form-container sign-in-container">
             <form action="#">
               <h1>Sign in</h1>
-              <input
-                type="text"
-                placeholder="Username"
-                value={username}
-                onChange={(event) => {
-                  setUsername(event.target.value)
-                }}
-              />
-              <input
-                type="password"
-                placeholder="Password"
-                value={password}
-                onChange={(event) => {
-                  setPassword(event.target.value)
-                }}
-              />
+              <div className="relative h-fit w-full">
+                <input
+                  type="text"
+                  placeholder="Username"
+                  value={username}
+                  onFocus={() => {
+                    if(wrongUser){
+                      setWrongUser(true)
+                    }
+                  }}
+                  onChange={(event) => {
+                    setUsername(event.target.value)
+                  }}
+                />
+                <div className="absolute inset-y-1/3 right-4 mt-1 h-1/5 w-3">
+                  {!wrongUser ? <></> : <FcHighPriority />}
+                </div>
+              </div>
+              <div className="relative h-fit w-full">
+                <input
+                  type="password"
+                  placeholder="Password"
+                  value={password}
+                  onFocus={() => {
+                    if(wrongPass){
+                      setWrongPass(true)
+                    }
+                  }}
+                  onChange={(event) => {
+                    setPassword(event.target.value)
+                  }}
+                />
+                <div className="absolute inset-y-1/3 right-4 mt-1 h-1/5 w-3">
+                  {!wrongPass ? <></> : <FcHighPriority />}
+                </div>
+              </div>
               <a href="#">Forgot your password?</a>
-              <button>Sign In</button>
+              <button onClick={login}>Sign In</button>
             </form>
           </div>
           <div className="overlay-container">
